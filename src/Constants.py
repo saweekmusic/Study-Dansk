1d8e4674da55f9a02a641db1e5e249536abb01bb
SAWEEK <saweek.official@gmail.com>
2025-07-01 16:02:13 +0200

    Moved section-connected constants inside the module

--- src/Constants.py
+++ src/Constants.py
@@ -3,28 +3,30 @@ import requests
 import fpdf
 from src.Functions.GetProxies import getProxies
 
-# Mapping English determiners to full Danish names
-EN_TO_DK = {
+# Word Generation Settigns
+TOPIC = None            # Topic for word generation, e.g., 'Animals', 'Food', etc.
+DU_LEVEL = None         # Danish level, e.g., 'A1', 'A2', etc.
+WORDS = []              # List to store generated words
+
+# Part-of-Speech Mappings
+EN_TO_DK = {            # English to full Danish POS names
     "noun": "substantiv",
     "verb": "verbum",
-    "adjective": "adjektiv",
-    "adverb": "adverbium"
+    "adjective": "adjektiv"
 }
 
-# Mapping full Danish determiners to sidebar abbreviations
-DK_TO_ABBR = {
+DK_TO_ABBR = {          # Danish POS names to sidebar abbreviations
     "substantiv": "sb.",
     "verbum": "vb.",
-    "adjektiv": "adj.",
-    "adverbium": "adv."
+    "adjektiv": "adj."
 }
 
-# For Scraping
+POS_TYPES = {           # UI/Display POS types to internal keys
+    "Nouns": "noun",
+    "Verbs": "verb",
+    "Adjectives": "adjective"
+}
+
+# Scraping Settings
 PROXIES = getProxies()
 UA = UserAgent()
-
-# Info that should be globally available
-TOPIC = str
-LEVEL = str
-TYPES = ['Nouns', 'Verbs', 'Adjectives']
-
--- src/Modules/Section.py
+++ src/Modules/Section.py
@@ -80,6 +80,9 @@ class Section:
         self.description = description
         self.body = body
 
+        self.title_size = 30
+        self.body_size = 11
+
     def add_body(self, body: Body):
         self.body.append(body)
 
@@ -90,17 +93,18 @@ class Section:
         # Additional rendering logic can be added here if needed
 
     def render_title(self):
-        self.pdf.set_font(family='helvetica-neue', style='B', size=30)
+        self.pdf.set_font(family='helvetica-neue', style='B', size=self.title_size)
         self.pdf.write(text=self.title)
         self.pdf.ln(2)
 
         # Line under the title
         with self.pdf.local_context(line_width=0.5):
             self.pdf.set_draw_color(r=179, g=179, b=179)
-            self.pdf.line(self.pdf.l_margin, 
-                      self.pdf.get_y(), 
-                      self.pdf.w - self.pdf.r_margin, 
-                      self.pdf.get_y())
+            self.pdf.line(
+                self.pdf.l_margin, 
+                self.pdf.get_y(), 
+                self.pdf.w - self.pdf.r_margin, 
+                self.pdf.get_y())
 
         self.pdf.ln(4)
 
