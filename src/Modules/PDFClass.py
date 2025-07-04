9022b6ae94b54d4edad2ef278189ef5e9623092b
SAWEEK <saweek.official@gmail.com>
2025-07-04 21:14:13 +0200

    Removed unused parts of code

--- src/Modules/PDFClass.py
+++ src/Modules/PDFClass.py
@@ -4,6 +4,7 @@ from src.Modules.MeaningClass import Meaning
 from fpdf import FPDF
 import random
 from wordsearch import WordSearch, Alphabets
+from src.Functions.AIrequests import *
 
 class PDF(FPDF):
     def __init__(self):
@@ -29,125 +30,6 @@ class PDF(FPDF):
                       style='BI', 
                       fname='src/Fonts/HelveticaNeue-BoldItalic-04.ttf',
                       uni=True)
-
-
-    def print_title(self, title: str):
-        self.set_font(family='helvetica-neue', style='B', size=30)
-        self.write(text=title)
-
-        self.print_gap(2)
-
-        # Print the line under the title
-        with self.local_context(line_width=0.5):
-            self.set_draw_color(r=179, g=179, b=179)
-            self.line(self.l_margin, 
-                      self.get_y(), 
-                      self.w - self.r_margin, 
-                      self.get_y())
-
-        self.ln(4)
-        
-
-    def print_subtitle(self, subtitle: str):
-        gap = 1.5
-
-        self.set_font(family='helvetica-neue', style='B', size=18)
-        self.cell(text=subtitle)
-
-        with self.local_context(text_mode='STROKE', line_width=0.5):
-            for i in range(0, 10):
-                self.set_x(self.get_x() + gap)
-                self.cell(text=subtitle)
-        
-        self.print_gap(2)
-
-
-    # Print_table function
-    def print_table(self, meanings: list[Meaning]):
-
-        # Set the font
-        self.set_font('helvetica-neue', size=11, style='')
-
-        # Follow the screenshot on your phone or on the website
-        with self.table(first_row_as_headings=False, 
-                        line_height=1.35 * self.font_size, 
-                        v_align='TOP', 
-                        padding=2, 
-                        markdown=True) as table:
-
-            for meaning in meanings:
-                row = table.row()
-                row.cell(meaning.definition_en)
-                row.cell(f"**{meaning.example}** / {meaning.example_en}")
-
-
-    # Print definition function
-    def print_definition(self, word: Word):
-
-        # Set the font
-        with self.local_context():
-
-            # Set the font
-            self.set_font(family='helvetica-neue', style='B', size=11)
-            
-            # Print the word
-            self.cell(text=f'{word.determiner} {word.word}' if word.determiner else f'{word.word}')
-
-        # Print the pronunciation
-        self.set_font(family='helvetica-neue', style='', size=11)
-        self.cell(text=word.pronunciation)
-        self.set_x(self.get_x() + 2)
-
-        # Get the width of the bending
-        bendings = '[' + ', '.join(bending for bending in word.bendings) + ']'
-        bending_width = self.get_string_width(bendings)
-
-        # Get the height of the text
-        height = self.font_size * 1.2
-
-        # Subtract the line width
-        height = height - 1.5
-
-        # Print the line
-        with self.local_context(line_width=0.5):
-            line_length = self.w - self.get_x() - self.r_margin - bending_width - 2
-
-            self.set_dash_pattern(dash=0.125, gap=4)
-            self.set_draw_color(r=179, g=179, b=179)
-            self.line(self.get_x(), 
-                      self.get_y() + height, 
-                      self.get_x() + line_length, 
-                      self.get_y() + height)
-
-        # Print the bendings
-        self.set_x(self.get_x() + line_length + 2)
-        self.cell(text=bendings)
-
-
-    def print_words(self, words: str, pos: str):
-
-        # For every word in the array words
-        for i, word in enumerate(words):
-
-            # If not the first word, set the gap between the words 5 mm
-            if i != 0:
-                self.ln(5) 
-
-            # Create a new word object
-            wordInit = Word(search_word=word, pos=pos)
-            WORDS.append(wordInit)
-
-            # Print the definition of the word
-            self.print_definition(word=wordInit)
-
-            # Gap
-            self.print_gap(2)
-
-            # Print the table of meanings
-            self.print_table(meanings=wordInit.meanings)
-
-        self.section_gap()
-
 
     def print_matches(self):
         words = []
@@ -250,18 +132,6 @@ class PDF(FPDF):
     # Gap between the sections
     def section_gap(self):
         self.ln(10)
-
-    
-    def calculate_table_height(self, table: CustomTable):
-        row_infos = table.get_row_layout_info()
-        if not row_infos:
-            return 0
-
-        total_height = sum(info.height for info in row_infos)
-        total_height += table._gutter_height * (len(row_infos) - 1)
-        total_height += 2 * table._outer_border_margin[1]
-        
-        return total_height
 
 
     
