fa671c3aac1230b43c636227eb227f8c5d5906f2
SAWEEK <saweek.official@gmail.com>
2025-07-04 16:25:46 +0200

    Implemented WordContent function

--- src/Modules/Body.py
+++ src/Modules/Body.py
@@ -1,15 +1,16 @@
-from src.Modules.PDFClass import PDF
-from fpdf.table import Table
-from Functions.Calculate import table_height
-from src.Constants import BODY_SIZE
+from src.Modules.WordClass import Word
+from src.Functions.Calculate import table_height
 from fpdf.enums import Align
+from fpdf.table import Table
+from src.Constants import BODY_SIZE
+from src.Modules.PDFClass import PDF
+from src.Functions.AIrequests import askWordsAI
+from src.Constants import WORDS
 
 class Body:
     def render(self):
         raise NotImplementedError("Subclasses should implement this method")
-    
-class WordContent(Body):
-    pass
+
 
 class TableContent(Body):
     def __init__(self, pdf: PDF, rows: list[list[str]], table: Table, square: bool = False) -> None:
@@ -26,8 +27,9 @@ class TableContent(Body):
             self.table._text_align = Align.coerce('C') # type: ignore
 
     def render(self):
-        self.pdf.set_font(family='helvetica', style='', size=BODY_SIZE)
+        self.pdf.set_font(family='helvetica-neue', style='', size=BODY_SIZE)
         self.table.render()
+
 
 class TextContent(Body):
     def __init__(self, pdf: PDF, text: str) -> None:
@@ -37,3 +39,66 @@ class TextContent(Body):
     def render(self):
         self.pdf.set_font(family='helvetica-neue', style='', size=BODY_SIZE)
         self.pdf.write(text=self.text)
+
+
+class WordContent(Body):
+    def __init__(self, pdf: PDF, DUlevel: str, topic: str, pos: str) -> None:
+        self.pdf = pdf
+        self.pos = pos
+        self.words = askWordsAI(DUlevel, topic, self.pos)
+
+    def subtitle(self, text: str):
+        horizontal_gap = 1.5
+
+        self.pdf.set_font(family='helvetica-neue', style='B', size=18)
+        self.pdf.cell(text=self.pos.capitalize())
+
+        with self.pdf.local_context(text_mode='STROKE', line_width=0.5):
+            for i in range(0, 10):
+                self.pdf.set_x(self.pdf.get_x() + horizontal_gap)
+                self.pdf.cell(text=self.pos.capitalize())
+
+    def wordInfo(self, word: Word):
+        # Print the word
+        self.pdf.set_font(family='helvetica-neue', style='B', size=11)
+        self.pdf.cell(text=f'{word.determiner} {word.word}' if word.determiner else f'{word.word}')
+
+        # Print the pronunciation
+        self.pdf.set_font(family='helvetica-neue', style='', size=11)
+        self.pdf.cell(text=word.pronunciation)
+        self.pdf.set_x(self.pdf.get_x() + 2)
+
+        # Calculate the line width
+        bendings = '[' + ', '.join(bending for bending in word.bendings) + ']'
+        bending_width = self.pdf.get_string_width(bendings)
+        height = self.pdf.font_size * 1.2
+        height = height - 1.5
+        line_length = self.pdf.w - self.pdf.get_x() - self.pdf.r_margin - bending_width - 2
+
+        # Print the line
+        with self.pdf.local_context(line_width=0.5):
+            self.pdf.set_dash_pattern(dash=0.125, gap=4)
+            self.pdf.set_draw_color(r=179, g=179, b=179)
+            self.pdf.line(
+                self.pdf.get_x(), 
+                self.pdf.get_y() + height, 
+                self.pdf.get_x() + line_length, 
+                self.pdf.get_y() + height)
+
+        # Print the bendings
+        self.pdf.set_x(self.pdf.get_x() + line_length + 2)
+        self.pdf.cell(text=bendings)
+
+    def render(self):
+        self.subtitle(self.pos)
+
+        for current_word in self.words:
+            WORDS.append(current_word)
+
+            word = Word(search_word=current_word, pos=self.pos)
+            self.wordInfo(word)
+            TableContent(
+                pdf = self.pdf, 
+                rows = [ [ meaning.definition_en, f"**{meaning.example}** / {meaning.example_en}" ] for meaning in word.meanings ],
+                table = Table(self.pdf, v_align='TOP', padding=2, markdown=True)
+            ).render()
